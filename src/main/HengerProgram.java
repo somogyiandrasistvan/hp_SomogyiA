959211dc6be3aaa57908061a2ce45cc4d1810aa3
somogyiandrasistvan <somogyiandraska@gmail.com>
2023-11-22 13:27:35 +0100

    UML elkészitése

---
+++ src/main/HengerProgram.java
@@ -0,0 +1,17 @@
+package main;
+
+import java.util.List;
+
+public class HengerProgram {
+
+    private List hengerek;
+
+    public static void main(String[] args) {
+        run();
+    }
+
+    private static void run() {
+        
+    }
+
+}
